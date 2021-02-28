064e1b69e0993476204720afc5c12bf923fd1f89
chenhh_x220 <chen1116@gmail.com>
2021-02-28 21:01:48 +0800

    new file:   "\346\245\232\347\213\202\344\272\272\346\212\225\350\263\207\350\207\264\345\257\214SOP2.mm" 	new file:   "\345\245\232\347\213\202\344\272\272\346\212\225\350\263\207\350\207\264\345\257\214SOP2.svg"

---
+++ 楚狂人投資致富SOP2.mm
@@ -0,0 +1,152 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="楚狂人投資致富SOP2" FOLDED="false" ID="ID_3301049" CREATED="1614504291263" MODIFIED="1614514540671" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<hook URI="../../../../C:/Users/chen1/Downloads/44386978986883760720145614090256.jpg" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="進場實作" POSITION="right" ID="ID_1314472760" CREATED="1614514407873" MODIFIED="1614514441166">
+<edge COLOR="#00ff00"/>
+<font SIZE="14"/>
+<node TEXT="下午1點後才是買賣點" ID="ID_166517269" CREATED="1614515684726" MODIFIED="1614515703185"/>
+<node TEXT="了解技術分析" ID="ID_9182043" CREATED="1614515712017" MODIFIED="1614515716611">
+<node TEXT="均線=投資人的成本" ID="ID_1350383463" CREATED="1614515722007" MODIFIED="1614515727419"/>
+<node TEXT="騰落指標：市場上漲股票數-下跌股票數, 為選中上漲股票的難易度" ID="ID_1399421084" CREATED="1614515767635" MODIFIED="1614515935388"/>
+</node>
+<node TEXT="預先規劃未來盤勢" ID="ID_1081899486" CREATED="1614515936291" MODIFIED="1614515958851" COLOR="#ff0000">
+<node TEXT="簡單原則：大盤沒有站穩季線3天以上不買股" ID="ID_372606275" CREATED="1614515978290" MODIFIED="1614516001099"/>
+</node>
+<node TEXT="選股：主升段最容易賺錢" ID="ID_343012092" CREATED="1614516031940" MODIFIED="1614516050383">
+<node TEXT="價量齊揚" ID="ID_686700828" CREATED="1614516051098" MODIFIED="1614516056491"/>
+<node TEXT="今天的高點是明天的低點" ID="ID_831784437" CREATED="1614516056989" MODIFIED="1614516067677"/>
+<node TEXT="不會出現長黑棒或跳空大跌的修正" ID="ID_1743137605" CREATED="1614516070863" MODIFIED="1614516083867"/>
+</node>
+<node TEXT="起漲後，可找爆量長紅+突破區間整理的個股交易" ID="ID_1624239734" CREATED="1614516116381" MODIFIED="1614516144656"/>
+</node>
+<node TEXT="迷思與謬誤" POSITION="right" ID="ID_966187147" CREATED="1614514427178" MODIFIED="1614514445998">
+<edge COLOR="#ff00ff"/>
+<font SIZE="14"/>
+<node TEXT="不要手癢操作" ID="ID_1503343579" CREATED="1614516166803" MODIFIED="1614516181288"/>
+<node TEXT="相對低點買進，相對高點賣出，不求買最低、賣最高。" ID="ID_1021594981" CREATED="1614516194612" MODIFIED="1614516404461" COLOR="#ff0000"/>
+<node TEXT="季線以上：做多；季線以下：做空。季線盤處時觀察。" ID="ID_507353914" CREATED="1614516229597" MODIFIED="1614516397398" COLOR="#ff0000"/>
+<node TEXT="EPS與PE為落後指標" ID="ID_207238511" CREATED="1614516275355" MODIFIED="1614516283770"/>
+<node TEXT="只買類別最強股" ID="ID_349754583" CREATED="1614516287177" MODIFIED="1614516297896"/>
+<node TEXT="停損是基本功" ID="ID_90358769" CREATED="1614516302923" MODIFIED="1614516306840"/>
+<node TEXT="股市下跌不需要任何理由" ID="ID_1684074547" CREATED="1614516315614" MODIFIED="1614516324630"/>
+<node TEXT="危機入市" ID="ID_1751897454" CREATED="1614516333282" MODIFIED="1614516338870">
+<node TEXT="1.獨佔性或有特殊利基的大公司" ID="ID_1364261460" CREATED="1614516339405" MODIFIED="1614516358870"/>
+<node TEXT="2.股價被誤殺" ID="ID_122297421" CREATED="1614516360010" MODIFIED="1614516368170"/>
+</node>
+</node>
+<node TEXT="先存到第一桶金" POSITION="left" ID="ID_1478897157" CREATED="1614514107210" MODIFIED="1614514191763">
+<edge COLOR="#ff0000"/>
+<font SIZE="16"/>
+<node TEXT="記帳" ID="ID_1355670943" CREATED="1614514567597" MODIFIED="1614514573280"/>
+<node TEXT="建立聚寶盆：多元收入" ID="ID_1680519851" CREATED="1614514585917" MODIFIED="1614514598750"/>
+<node TEXT="被動收入：寫書、網誌、小說、app、收房租、建網站" ID="ID_353654359" CREATED="1614514612949" MODIFIED="1614514653125"/>
+<node TEXT="增加年收：在原公司建立價值、辭職創業、發展副業" ID="ID_537142235" CREATED="1614514677747" MODIFIED="1614514712108"/>
+<node TEXT="穩定的工作-&gt;風險較高的投資" ID="ID_1112229935" CREATED="1614514727150" MODIFIED="1614514740522"/>
+<node ID="ID_1218204045" CREATED="1614514762338" MODIFIED="1614514781327" COLOR="#ff3333"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>績效決定工作收入，而不是時間</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="建立正確的投資觀念" POSITION="left" ID="ID_73328697" CREATED="1614514379274" MODIFIED="1614514437775">
+<edge COLOR="#0000ff"/>
+<font SIZE="14"/>
+<node TEXT="散戶：不知道為何買股票、借錢買股票、小賺大賠、亂用技術分析、不信邪、沒有資金控管" ID="ID_1802961256" CREATED="1614514801918" MODIFIED="1614514855972"/>
+<node TEXT="贏家特質：耐心。重小利者不易有大成就" ID="ID_605968155" CREATED="1614514868935" MODIFIED="1614514893534"/>
+<node TEXT="累積資金、耐心、經常研究、不要過度分散投資(3~5隻)、好的變現性、寡佔或企業價值高、全球化" ID="ID_1256882937" CREATED="1614514911050" MODIFIED="1614514988464" COLOR="#ff3333"/>
+<node TEXT="賠錢不要找藉口" ID="ID_1337354204" CREATED="1614514997049" MODIFIED="1614515006432"/>
+<node TEXT="投資：巴菲特。投機：索羅斯、科斯托藍尼" ID="ID_1274943709" CREATED="1614515013583" MODIFIED="1614515036011"/>
+<node TEXT="新手：不怕死，經常all in。融資、攤平、跟單-&gt;陣亡。&#xa;賺不了錢的老手：小賺大賠。老手 != 贏家。&#xa;賺錢的高手：&#xa;可接受不是賣最高與買最低、不會手癢進場、情緒控管好" ID="ID_1617178861" CREATED="1614515060481" MODIFIED="1614515251346" COLOR="#0000f8"/>
+<node TEXT="順勢操作：投機。&#xa;逆勢操作：投資，彎腰撿鑽石" ID="ID_401220986" CREATED="1614515155582" MODIFIED="1614515193077"/>
+<node TEXT="資金控管：高風險的資產資金放越少" ID="ID_955424232" CREATED="1614515301952" MODIFIED="1614515335294"/>
+</node>
+<node TEXT="小提醒" POSITION="right" ID="ID_216721600" CREATED="1614515372926" MODIFIED="1614515376711">
+<edge COLOR="#00ffff"/>
+<node TEXT="年初至年底買景氣循環股勝率高" ID="ID_148202553" CREATED="1614515377503" MODIFIED="1614515409285"/>
+<node TEXT="10~11月低點做多，過年前勝率高" ID="ID_642542374" CREATED="1614515414929" MODIFIED="1614515427513"/>
+<node TEXT="年底做帳機會多，低買勝率高" ID="ID_410729822" CREATED="1614515434059" MODIFIED="1614515454296"/>
+<node TEXT="通路股年底通常營收好，勝率高" ID="ID_852676552" CREATED="1614515459728" MODIFIED="1614515471742"/>
+<node TEXT="高位爆大量個股做空勝率高" ID="ID_1470468315" CREATED="1614515475754" MODIFIED="1614515499500"/>
+<node TEXT="離季線乖離40%過大不要買" ID="ID_1082636555" CREATED="1614515506714" MODIFIED="1614515533251"/>
+<node TEXT="5日與20日均量黃金交叉，多頭加分" ID="ID_1816346446" CREATED="1614515537708" MODIFIED="1614515561587"/>
+<node TEXT="政府護盤為短多中空長多格局" ID="ID_1834205050" CREATED="1614515574105" MODIFIED="1614515595018"/>
+<node TEXT="常發新聞的公司少碰" ID="ID_313773951" CREATED="1614515599637" MODIFIED="1614515608358"/>
+<node TEXT="周轉率超過10%的公司有問題" ID="ID_42357369" CREATED="1614515612713" MODIFIED="1614515624993"/>
+<node TEXT="景氣不會影響漲跌，只有趨勢會影響漲跌" ID="ID_1184340723" CREATED="1614515645952" MODIFIED="1614515660062"/>
+</node>
+</node>
+</map>
