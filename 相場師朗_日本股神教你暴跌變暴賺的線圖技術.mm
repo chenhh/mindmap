c2c1d1f630205d560fa3aa2632eadca911073234
chenhh_x220 <chen1116@gmail.com>
2021-03-07 12:19:09 +0800

    add

---
+++ 相場師朗_日本股神教你暴跌變暴賺的線圖技術.mm
@@ -0,0 +1,107 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="日本股神教你暴跌變暴賺的線圖技術" FOLDED="false" ID="ID_807898336" CREATED="1614659476672" MODIFIED="1614659543689" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<hook URI="cover/日本股神教你暴跌變暴賺的線圖技術.jpg" SIZE="0.49526834" NAME="ExternalObject"/>
+<node TEXT="相場師朗" POSITION="right" ID="ID_974937319" CREATED="1614659612967" MODIFIED="1614659625078">
+<edge COLOR="#ff0000"/>
+<node TEXT="放空與技術線形" ID="ID_1616160583" CREATED="1614659625648" MODIFIED="1614659641910"/>
+<node TEXT="技術指標" ID="ID_753307906" CREATED="1614659686198" MODIFIED="1614659699034"/>
+<node TEXT="" ID="ID_1409953143" CREATED="1614659815515" MODIFIED="1614659824356">
+<hook URI="cover/相場師朗_照片.png" SIZE="0.33434275" NAME="ExternalObject"/>
+</node>
+<node TEXT="與是川銀藏(價值投資)操盤差異很大，以技術面為主" ID="ID_379743013" CREATED="1614684244916" MODIFIED="1614684283802"/>
+</node>
+<node TEXT="放空" POSITION="left" ID="ID_1118806301" CREATED="1614684162002" MODIFIED="1614684172463">
+<edge COLOR="#0000ff"/>
+<node TEXT="相場6成獲利來自於放空" ID="ID_695885306" CREATED="1614684366359" MODIFIED="1614684377901"/>
+<node ID="ID_513790349" CREATED="1614684445622" MODIFIED="1614684470267" COLOR="#ff0000"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>股市本質：漲上去的必定跌，跌下去的不一定漲回來</b>。
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="無論什麼樣的上漲行情，都存在許多下跌局面" ID="ID_451522961" CREATED="1614684497730" MODIFIED="1614684516748">
+<node TEXT="嘗試攻頂失敗" ID="ID_360539327" CREATED="1614684535726" MODIFIED="1614684560365"/>
+<node TEXT="整數關卡(行情通常特別大)" ID="ID_1750295480" CREATED="1614684561398" MODIFIED="1614684578414"/>
+</node>
+</node>
+<node TEXT="均線(moving average)" POSITION="right" ID="ID_114076623" CREATED="1614684173798" MODIFIED="1614684185193">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="融資券" POSITION="left" ID="ID_1794064423" CREATED="1614684190899" MODIFIED="1614684195373">
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
