053b09cfa1c0170e933ddbe80a9aed578c5079b1
chenhh_x220 <chen1116@gmail.com>
2021-03-22 21:45:37 +0800

    選擇權

---
+++ 財務報表.mm
@@ -0,0 +1,100 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="財務報表" FOLDED="false" ID="ID_1767646460" CREATED="1616311828796" MODIFIED="1616311840137" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="資產負債表&#xa;(balance sheet)" POSITION="right" ID="ID_1913733606" CREATED="1616311890754" MODIFIED="1616311907527">
+<edge COLOR="#ff0000"/>
+<node TEXT="靜態，公司的資金來源或去處" ID="ID_244348286" CREATED="1616311978931" MODIFIED="1616311992619"/>
+<node TEXT="資產=負債+股東權益" ID="ID_1794801474" CREATED="1616312002731" MODIFIED="1616312013434"/>
+</node>
+<node TEXT="損益表&#xa;(income statement)" POSITION="right" ID="ID_1824799722" CREATED="1616311908477" MODIFIED="1616311922884">
+<edge COLOR="#0000ff"/>
+<node TEXT="動態" ID="ID_1496266414" CREATED="1616312024738" MODIFIED="1616312065847"/>
+<node TEXT="營業收入+營業外收入-所得稅=稅後淨利" ID="ID_547139430" CREATED="1616312066157" MODIFIED="1616312066953"/>
+</node>
+<node TEXT="股東權益變動表&#xa;(statement of stockholders equity)" POSITION="right" ID="ID_610432489" CREATED="1616311927124" MODIFIED="1616311957011">
+<edge COLOR="#00ff00"/>
+<node TEXT="股東權利可再分解為股本、資本公積、保留盈餘、其它權益" ID="ID_549950488" CREATED="1616312110905" MODIFIED="1616312145850"/>
+</node>
+<node TEXT="現金流量表&#xa;(cashflow statement)" POSITION="right" ID="ID_146162597" CREATED="1616311937377" MODIFIED="1616311947090">
+<edge COLOR="#ff00ff"/>
+<node ID="ID_681775846" CREATED="1616312078011" MODIFIED="1616312098368"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      將資產負債表中的現金及約當現金，拆解為<b>營業、投資及籌(融)資</b>三個活動分類
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</map>
