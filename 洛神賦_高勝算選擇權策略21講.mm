f91a036794f5dd109a2c24553f4f7b4d7d4b81d4
chenhh_x220 <chen1116@gmail.com>
2021-03-25 11:04:11 +0800

    選擇權

---
+++ 洛神賦_高勝算選擇權策略21講.mm
@@ -0,0 +1,131 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="高勝算選擇權策略21講" FOLDED="false" ID="ID_380749181" CREATED="1616420928972" MODIFIED="1616421008511" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<hook URI="cover/高勝算選擇權策略21講.jpg" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="OP投資原則" POSITION="right" ID="ID_1053662240" CREATED="1616421149147" MODIFIED="1616421161443">
+<edge COLOR="#ff0000"/>
+<node TEXT="莊家(賣方)比較容易賺錢" ID="ID_362823390" CREATED="1616421202035" MODIFIED="1616421454375" COLOR="#ff3333">
+<font BOLD="true"/>
+</node>
+<node TEXT="要有進出場的機制" ID="ID_198354548" CREATED="1616421221611" MODIFIED="1616421228006"/>
+<node TEXT="要嚴格執行交易策略" ID="ID_431741656" CREATED="1616421236683" MODIFIED="1616421240957"/>
+<node TEXT="讀書" ID="ID_1240561460" CREATED="1616421299206" MODIFIED="1616421302286">
+<node TEXT="國外的書多是股票選擇權，而台灣主流是指數選擇權" ID="ID_1927298923" CREATED="1616421302752" MODIFIED="1616421318866"/>
+<node TEXT="參考期貨商交易報告，學習如何分析多空" ID="ID_872579134" CREATED="1616421326688" MODIFIED="1616421343444"/>
+<node TEXT="交易中了解自已的性格，找出適合自已的策略" ID="ID_739385728" CREATED="1616421349770" MODIFIED="1616421361313"/>
+<node TEXT="資金一定要控管好，要有出場點" ID="ID_1845128747" CREATED="1616421365468" MODIFIED="1616421375665"/>
+</node>
+</node>
+<node TEXT="買方策略" POSITION="left" ID="ID_1445290257" CREATED="1616639975285" MODIFIED="1616639978556">
+<edge COLOR="#0000ff"/>
+<node TEXT="大賺小賠，最多歸0" ID="ID_1345920279" CREATED="1616640048439" MODIFIED="1616640096044">
+<node TEXT="單純買call, put" ID="ID_807562392" CREATED="1616640158310" MODIFIED="1616640163480"/>
+<node TEXT="技術指標進場" ID="ID_1825741717" CREATED="1616640176045" MODIFIED="1616640182078"/>
+<node TEXT="價差單賭結算" ID="ID_929737496" CREATED="1616640191249" MODIFIED="1616640195865"/>
+</node>
+<node TEXT="低勝率，高報酬，要算好期望值" ID="ID_1446784812" CREATED="1616640224139" MODIFIED="1616640241315"/>
+<node TEXT="進場點、出場點、資金控管" ID="ID_1455542098" CREATED="1616640131072" MODIFIED="1616640144316"/>
+</node>
+<node TEXT="賣方策略" POSITION="right" ID="ID_1213870117" CREATED="1616640214911" MODIFIED="1616640220507">
+<edge COLOR="#00ff00"/>
+<node TEXT="莊家，高勝率，低報酬" ID="ID_594315143" CREATED="1616640259463" MODIFIED="1616640272075"/>
+<node TEXT="賺時間價值與波動" ID="ID_397071185" CREATED="1616640296313" MODIFIED="1616640306507"/>
+</node>
+<node TEXT="盤整後的突破" POSITION="left" ID="ID_708528973" CREATED="1616640474399" MODIFIED="1616640487986">
+<edge COLOR="#ff00ff"/>
+<node TEXT="比例價差" ID="ID_1216319336" CREATED="1616640498429" MODIFIED="1616640503530">
+<node TEXT="目前在盤整格局，上漲不易，但可能結束盤整變成大跌" ID="ID_1125413719" CREATED="1616640504083" MODIFIED="1616640534199"/>
+</node>
+<node TEXT="逆比例價差" ID="ID_1394099494" CREATED="1616640542540" MODIFIED="1616640560788">
+<node TEXT="猜測會有突破性的大漲，但又怕破不了壓力線產生假突破下殺" ID="ID_218412801" CREATED="1616640575095" MODIFIED="1616640606163"/>
+</node>
+</node>
+<node TEXT="技術分析" POSITION="right" ID="ID_209678782" CREATED="1616640641533" MODIFIED="1616640647788">
+<edge COLOR="#00ffff"/>
+<node TEXT="突破訊號後返回: buy call" ID="ID_1104349085" CREATED="1616640656475" MODIFIED="1616640671414"/>
+<node TEXT="三角形收斂或雙重頂完成: sell call" ID="ID_1047968976" CREATED="1616640671852" MODIFIED="1616640688502"/>
+<node TEXT="頭肩頸突破頸線之前：bull spread" ID="ID_1981839726" CREATED="1616640689602" MODIFIED="1616640720222"/>
+<node TEXT="波浪的第2或5波：時間價差(短空長多)" ID="ID_1111931126" CREATED="1616640720560" MODIFIED="1616640746071"/>
+<node TEXT="趨勢線收斂：蝶式價差" ID="ID_1528441025" CREATED="1616640746843" MODIFIED="1616640783548"/>
+<node TEXT="形態" ID="ID_347602176" CREATED="1616640784651" MODIFIED="1616640792257">
+<node TEXT="盤整" ID="ID_455331355" CREATED="1616640792643" MODIFIED="1616640797375">
+<node TEXT="可預期7日內變盤" ID="ID_330746213" CREATED="1616640804591" MODIFIED="1616640812888"/>
+</node>
+<node TEXT="趨勢線與支撐、壓力線" ID="ID_1720129789" CREATED="1616640823219" MODIFIED="1616640834175"/>
+</node>
+<node TEXT="put/call ratio" ID="ID_1145463932" CREATED="1616641124309" MODIFIED="1616641128325">
+<node TEXT="下跌時，通常pc&lt;1" ID="ID_174186500" CREATED="1616641128989" MODIFIED="1616641150336"/>
+<node TEXT="上漲時，通常pc&gt;1" ID="ID_1964063564" CREATED="1616641150729" MODIFIED="1616641157403"/>
+</node>
+</node>
+<node TEXT="波動率" POSITION="left" ID="ID_1667658822" CREATED="1616640897554" MODIFIED="1616640900088">
+<edge COLOR="#7c0000"/>
+<node TEXT="期交所有每分鐘的波動率指數" ID="ID_1286969275" CREATED="1616640904516" MODIFIED="1616640920885"/>
+<node TEXT="高波動率時適合做賣方" ID="ID_1681623237" CREATED="1616640924697" MODIFIED="1616640931336"/>
+<node TEXT="大漲或大跌時波動率高，盤整時波動率低" ID="ID_876722810" CREATED="1616640953789" MODIFIED="1616640972401"/>
+</node>
+</node>
+</map>
