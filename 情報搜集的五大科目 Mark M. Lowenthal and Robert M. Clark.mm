dcd36ccd8164c4b44bf1c154fffd6f5656dee48d
chenhh <chenhh@eva01>
2024-02-20 14:09:49 +0800

    情報搜尋的五大科目

---
+++ 情報搜集的五大科目 Mark M. Lowenthal and Robert M. Clark.mm
@@ -0,0 +1,135 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="情報搜集的五大科目" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1708406587416" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10" RULE="ON_BRANCH_CREATION"/>
+<hook URI="file:///home/chenhh/workspace_plot/mindmap/cover/情報搜集的五大科目_cover.png" SIZE="0.48857144" NAME="ExternalObject"/>
+<node TEXT="開源情報(OSINT)" POSITION="right" ID="ID_534720979" CREATED="1708405665066" MODIFIED="1708405914468">
+<edge COLOR="#ff0000"/>
+<font SIZE="20"/>
+<cloud COLOR="#ff9999" SHAPE="ARC"/>
+</node>
+<node TEXT="人力情報(HUMINT)" POSITION="left" ID="ID_1798707436" CREATED="1708405685994" MODIFIED="1708405948129">
+<edge COLOR="#0000ff"/>
+<font SIZE="20"/>
+<cloud COLOR="#ffff99" SHAPE="ARC"/>
+</node>
+<node TEXT="信號情報(SIGINT)" POSITION="right" ID="ID_61198248" CREATED="1708405699914" MODIFIED="1708405964209">
+<edge COLOR="#00ff00"/>
+<font SIZE="20"/>
+<cloud COLOR="#6666ff" SHAPE="ARC"/>
+</node>
+<node TEXT="地理空間情報(GEOINT)" POSITION="left" ID="ID_1708312860" CREATED="1708405720218" MODIFIED="1708405957266">
+<edge COLOR="#ff00ff"/>
+<font SIZE="20"/>
+<cloud COLOR="#66cc00" SHAPE="ARC"/>
+</node>
+<node TEXT="測量與特徵情報(MASINT)" POSITION="right" ID="ID_1877268880" CREATED="1708405737226" MODIFIED="1708405937714">
+<edge COLOR="#00ffff"/>
+<font SIZE="20"/>
+<cloud COLOR="#009999" SHAPE="ARC"/>
+</node>
+<node TEXT="搜集管理" POSITION="left" ID="ID_32143941" CREATED="1708405856834" MODIFIED="1708405865138">
+<edge COLOR="#00007c"/>
+<font SIZE="20"/>
+<node TEXT="TPED/TCPED流程" ID="ID_1547896049" CREATED="1708405970985" MODIFIED="1708406397124">
+<font SIZE="20"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="任務" ID="ID_1893723135" CREATED="1708405995545" MODIFIED="1708406397128">
+<font SIZE="20"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+</node>
+<node TEXT="搜集" ID="ID_1386456803" CREATED="1708406012345" MODIFIED="1708406397127">
+<font SIZE="20"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+</node>
+<node TEXT="處理" ID="ID_103356043" CREATED="1708406128793" MODIFIED="1708406397127">
+<font SIZE="20"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+</node>
+<node TEXT="開發" ID="ID_1114902360" CREATED="1708406137001" MODIFIED="1708406397127">
+<font SIZE="20"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+</node>
+<node TEXT="分發" ID="ID_245013182" CREATED="1708406148041" MODIFIED="1708406397128">
+<font SIZE="20"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+</node>
+</node>
+</node>
+<node TEXT="術語" POSITION="right" ID="ID_1246617189" CREATED="1708406207290" MODIFIED="1708406213346">
+<edge COLOR="#7c007c"/>
+<font SIZE="20"/>
+<node TEXT="秘密(clandstine): 保密可歸因/隱蔽(covert)：保密不可歸因。" ID="ID_1668569920" CREATED="1708406214826" MODIFIED="1708406266594"/>
+<node TEXT="附屬(collateral)：不屬於該組織的材料和信息。" ID="ID_859961795" CREATED="1708406276249" MODIFIED="1708406299068"/>
+<node TEXT="搜集(collection)：整個過程或實際獲取信息和情報的環節。" ID="ID_1844825366" CREATED="1708406301225" MODIFIED="1708406332330"/>
+<node TEXT="多科目分析(multi-INT)/全源分析(all-source)" ID="ID_459405246" CREATED="1708406334105" MODIFIED="1708406381679"/>
+</node>
+</node>
+</map>
